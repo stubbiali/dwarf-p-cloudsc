1deb68cdd7d938e4a1bfb40747ccb015026041ed
stubbiali <subbiali@phys.ethz.ch>
2023-07-19 13:40:34 +0200

    Remove deprecated files.

--- src/cloudsc_python/src/cloudsc4py/physics/_stencils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import cloudsc4py.physics._stencils.cloudsc
-import cloudsc4py.physics._stencils.cloudsc_fused
-import cloudsc4py.physics._stencils.cloudsc_split
-import cloudsc4py.physics._stencils.cuadjtq
-import cloudsc4py.physics._stencils.fccld
-import cloudsc4py.physics._stencils.fcttre
-import cloudsc4py.physics._stencils.helpers
